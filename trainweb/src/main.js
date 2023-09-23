8ccc1ff410981889dc5741d6a91c950e73eaa471
chang <1241236298@qq.com>
2023-09-23 19:56:34 +0800

    整合Ant Design Vue

--- trainweb/src/main.js
+++ trainweb/src/main.js
@@ -2,8 +2,17 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+// 引入图标组件
+import * as Icons from '@ant-design/icons-vue';
 
 // 通过main.js(框架配置入口), 将index.html(html页面入口)和App.Vue(Vue页面入口)关联起来。
 // <div id="app"></div>  index.html
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(store).use(router).use(Antd).mount('#app')
+
+const icons = Icons;
+for (const i in icons) {
+    app.component(i, icons[i])
+}
