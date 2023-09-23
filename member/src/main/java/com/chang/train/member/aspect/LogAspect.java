77d0d4ea35b4673b33dd00ed2d70de1cc5e8deef
chang <1241236298@qq.com>
2023-09-23 14:07:33 +0800

    增加LogAspect: 请求日志参数 + 方法的调用时间

---
+++ member/src/main/java/com/chang/train/member/aspect/LogAspect.java
@@ -0,0 +1,99 @@
+package com.chang.train.member.aspect;
+
+import cn.hutool.core.util.RandomUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.support.spring.PropertyPreFilters;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+@Aspect
+@Component
+public class LogAspect {
+    public LogAspect() {
+        System.out.println("Common LogAspect");
+    }
+
+    private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
+
+    /**
+     * 定义一个切点: 写的这个类在哪里起作用,controller后缀类名下的所有的方法。
+     */
+    @Pointcut("execution(public * com.chang..*Controller.*(..))")
+    public void controllerPointcut() {
+    }
+
+    // Before 调用方法的前面，就会执行增强的方法。
+    @Before("controllerPointcut()")
+    public void doBefore(JoinPoint joinPoint) {
+
+        // 增加日志流水号, 唯一的流水号即可。 系统参数 + 随机的三位字符串。
+        // 1695449092491kuu 一次请求的日志流水号。 同一个流水号的，就是同一个请求。
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+
+        // 开始打印请求日志
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        Signature signature = joinPoint.getSignature();
+        // 方法名
+        String name = signature.getName();
+
+        // 打印请求信息
+        LOG.info("------------- 开始 -------------");
+        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("类名方法: {}.{}", signature.getDeclaringTypeName(), name);
+        LOG.info("远程地址: {}", request.getRemoteAddr());
+
+        // 打印请求参数
+        Object[] args = joinPoint.getArgs();
+        // LOG.info("请求参数: {}", JSONObject.toJSONString(args));
+
+        // 排除特殊类型的参数，如文件类型
+        Object[] arguments = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest
+                    || args[i] instanceof ServletResponse
+                    || args[i] instanceof MultipartFile) {
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        // 排除字段，敏感字段或太长的字段不显示：身份证、手机号、邮箱、密码等
+        // 自己开发的系统，自己应该也不知道一些敏感信息，自己也破解不了， 保障客户信息安全。
+        String[] excludeProperties = {};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("请求参数: {}", JSONObject.toJSONString(arguments, excludefilter));
+    }
+
+    // 方法的运行的时间。 在调用方法的开始和结束的时候。
+    @Around("controllerPointcut()")
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        // 排除字段，敏感字段或太长的字段不显示：身份证、手机号、邮箱、密码等
+        String[] excludeProperties = {};
+        PropertyPreFilters filters = new PropertyPreFilters();
+        PropertyPreFilters.MySimplePropertyPreFilter excludefilter = filters.addFilter();
+        excludefilter.addExcludes(excludeProperties);
+        LOG.info("返回结果: {}", JSONObject.toJSONString(result, excludefilter));
+        LOG.info("------------- 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+        return result;
+    }
+
+}
