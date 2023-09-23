a325528dc704b91b6f4f9321ddd9faa1e732642d
chang <1241236298@qq.com>
2023-09-23 13:47:32 +0800

    修改启动日志，增加项目路径/member，配置logback

--- member/src/main/java/com/chang/train/member/MemberApplication.java
+++ member/src/main/java/com/chang/train/member/MemberApplication.java
@@ -1,7 +1,12 @@
 package com.chang.train.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
 
 /**
  * @title: MemberApplication
@@ -11,7 +16,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(MemberApplication.class, args);
+
+        // 获取配置文件里面的参数
+        ConfigurableEnvironment environment = run.getEnvironment();
+        LOG.info("启动成功!!");
+        LOG.info("测试地址: \t http://127.0.0.1:{}{}/hello", environment.getProperty("server.port"), environment.getProperty("server.servlet.context-path"));
+
     }
 }
